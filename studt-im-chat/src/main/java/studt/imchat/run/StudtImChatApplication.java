2ac6c811f74fc6bd36126fb2d69e8b3db01b57b0
smart <yangxuan_321@163.com>
2018-09-29 18:03:06 +0800

    1.新建 studt-im-chat工程。 2.设计客户端与服务端的通信协议设计

---
+++ studt-im-chat/src/main/java/studt/imchat/run/StudtImChatApplication.java
@@ -0,0 +1,12 @@
+package studt.imchat.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudtImChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudtImChatApplication.class, args);
+    }
+}
