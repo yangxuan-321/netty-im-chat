878eef81ae314d68b94bfdcb001fc27113bc1696
smart <yangxuan_321@163.com>
2019-03-01 18:29:52 +0800

    Filter设计 -- 责任链模式 验证登录

---
+++ netty-im-chat-server/src/main/java/com/netty/imchat/servicefilter/LoginFilter.java
@@ -0,0 +1,30 @@
+package com.netty.imchat.servicefilter;
+
+import com.netty.imchat.common.entity.packet.Packet;
+import com.netty.imchat.filter.Filter;
+import com.netty.imchat.filter.FilterChain;
+import com.netty.imchat.filter.FilterConfig;
+
+/**
+ * @author Kevin
+ * @Title: LoginFilter
+ * @ProjectName netty-im-chat
+ * @Description: TODO
+ * @date 2019/3/1 14:21
+ */
+public class LoginFilter implements Filter<Packet> {
+    @Override
+    public void init(FilterConfig paramFilterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(Packet packet, FilterChain filterChain) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
